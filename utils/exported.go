2e28eaf4315d9700e8e750ba7550c056b4ad9491
Zheeeng <hi@zheeeng.me>
2019-03-13 17:07:08 +0800

    Update exported.go

--- utils/exported.go
+++ utils/exported.go
@@ -1,14 +1,14 @@
 package utils
 
+// For test 301 direction prevention
 const (
 	// ExportedIndexPattern for test
 	ExportedIndexPattern = indexPattern
-	// ExportedSuffixSlashPattern for test
-	ExportedSuffixSlashPattern = suffixSlashPattern
 	// ExportedDelimiterPattern for test
 	ExportedDelimiterPattern = delimiterPattern
 )
 
+// For url validation
 var (
 	// ExportedBaseExp validates a URL matches the pattern of Github folder URI
 	ExportedBaseExp = baseExp
